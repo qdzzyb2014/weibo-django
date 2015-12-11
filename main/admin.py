01d226b84ee2673675861f9464e50fbf77820944
隔壁小王 <qdzzyb2015@gmail.com>
2015-12-11 20:47:32 +0800

    add Role in admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import User
+from main.models import User, Role
 
 admin.site.register(User)
+admin.site.register(Role)
