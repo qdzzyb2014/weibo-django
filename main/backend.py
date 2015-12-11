44f3871ac751825958ddd0a3289a82addf98239c
隔壁小王 <qdzzyb2015@gmail.com>
2015-12-11 20:48:22 +0800

    delete UsernameAuthBackend

--- main/backend.py
+++ main/backend.py
@@ -22,20 +22,3 @@ class EmailAuthBackend(object):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-
-
-class UsernameAuthBackend(object):
-
-    def authenticate(self, username=None, password=None):
-        try:
-            user = User.objects.get(username=username)
-            if user.check_password(password):
-                return user
-        except User.DoesNotExist:
-            return None
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
