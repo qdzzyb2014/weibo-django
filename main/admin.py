e7c9a304c18e0de8a3812a4bb2507e4324af2f8d
隔壁小王 <qdzzyb2015@gmail.com>
2015-12-13 21:19:35 +0800

    改变admin注册方式

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import User, Role
+from main.models import User, Role, Post
 
+'''
 admin.site.register(User)
 admin.site.register(Role)
+admin.site.register(Post)
+'''
+registe_list = [User, Role, Post]
+for i in registe_list:
+    admin.site.register(i)
